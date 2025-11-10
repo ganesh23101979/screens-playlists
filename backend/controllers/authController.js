0489385fcf9a2af3a2bd1a0a9726e9b7e759aecf
ganesh23101979 <ganesh.santhosh201@gmail.com>
2025-11-10 19:25:57 +0530

    screens-playlists

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,4 @@
-import Joi from "joi";
+
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,5 +1,4 @@
 import jwt from "jsonwebtoken";
-import User from "../models/User.js";
 
 export function authOptional(req, res, next) {
   const auth = req.headers.authorization;
